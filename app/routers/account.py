a557f609123baa4cf6f0cfd626f84e7ce3010f5b
Joseph Ibok <josephibok75@gmail.com>
2026-04-24 12:33:47 +0100

    Validated role for user sign up, which prevents users from assigning admin role

--- app/routers/account.py
+++ app/routers/account.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, status
+from app.schemas.account_schema import SignupDto
 
 router = APIRouter(prefix="/account", tags=["Authentication"])
 
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED)
-def create_new_user():
-    return {"message": "User successfully created"}
+def create_new_user(payload: SignupDto):
+    return {"payload": payload}
--- app/schemas/account_schema.py
+++ app/schemas/account_schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field, field_validator
 
 from .user_schema import (
     UserRead,
@@ -9,4 +9,36 @@ from .user_schema import (
 
 class Token(BaseModel):
     access_token: str
+    refresh_token: str
     token_type: str
+
+
+class SignupDto(BaseModel):
+    email: EmailStr
+    role: UserRole = Field(..., examples=[UserRole.READER])
+    username: str = Field(..., min_length=5)
+    password: str = Field(..., min_length=8,
+                          description="Password must be at least 8 characters long")
+
+    @field_validator("role")
+    def validate_role(cls, v):
+        allowed_roles = {UserRole.AUTHOR, UserRole.READER}
+
+        if v not in allowed_roles:
+            raise ValueError(f"Role '{v}' is not allowed for sign-up")
+        return v
+
+    @field_validator("password")
+    def validate_password(cls, v):
+        if not any(char.isupper() for char in v):
+            raise ValueError(
+                "Password must contain at least one uppercase letter")
+        if not any(char.islower() for char in v):
+            raise ValueError(
+                'Password must contain at least one lowercase letter')
+        if not any(char.isdigit() for char in v):
+            raise ValueError('Password must contain at least one digit')
+        if not any(char in "!@#$%^&*()-_=+[]{}|;:,.<>?/" for char in v):
+            raise ValueError(
+                'Password must contain at least one special character')
+        return v
