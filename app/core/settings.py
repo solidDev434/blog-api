213c054a7ad362f594e91fac1230f2a71bb2f2ad
Joseph Ibok <josephibok75@gmail.com>
2026-04-27 13:40:12 +0100

    Configured and implemented Email service in the required auth services

--- app/core/settings.py
+++ app/core/settings.py
@@ -8,6 +8,17 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     JWT_REFRESH_KEY: str
 
+    SERVER_URL: str
+
+    # Mail
+    MAIL_USER: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_HOST: str
+    MAIL_STARTTLS: bool
+    MAIL_SSL_TLS: bool
+
     model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 
